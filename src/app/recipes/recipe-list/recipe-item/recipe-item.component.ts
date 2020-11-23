1ae1677f359ea9588525d66148edf66333db3385
Chirag Shah <chirag.crimson@gmail.com>
2020-11-23 15:16:55 +0530

    Recipe model added

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './../recipe.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css'],
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() selectedRecipe = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelected() {
+    this.selectedRecipe.emit();
+  }
+}
