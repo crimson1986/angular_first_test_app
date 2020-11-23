9bb9020b0f22f3c4112f6d9f23228f5d06fe52dc
Chirag Shah <chirag.crimson@gmail.com>
2020-11-23 15:16:43 +0530

    Recipe model added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  loadedScreen = 'recipe';
   title = 'first-app';
+
+  onNavigationSelected(screen: string) {
+    this.loadedScreen = screen;
+  }
 }
