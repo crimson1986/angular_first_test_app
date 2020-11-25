06aeba33127c770d5e7cfd6ae3fb7e5b0d44a5d0
Chirag Shah <chirag.crimson@gmail.com>
2020-11-25 17:15:24 +0530

    Code changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe-list/recipe.model';
 
 export class RecipeService {
@@ -6,14 +7,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'Hot chocolate',
+      'Who does not like chocolate?',
+      'https://assets.bonappetit.com/photos/57accdd1f1c801a1038bc794/16:9/w_2560%2Cc_limit/Hot-Chocolate-2-of-5.jpg',
+      [new Ingredient('milk', 1), new Ingredient('Chocolate', 2)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'Sandwich',
+      'Healthy break-fast',
+      'https://www.residentadvisor.net/images/events/flyer/2016/4/jp-0406-819275-front.jpg',
+      [new Ingredient('Bread', 2), new Ingredient('Onion', 1)]
     ),
   ];
 
